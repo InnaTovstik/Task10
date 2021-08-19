c19ce2bac4fb378d159dd7bfd1ffcc4202c1e2e6
InnaTovstik <kitimail69@gmail.com>
2021-08-19 18:23:21 +0300

    Add code

--- src/com/company/EmployeeSalaryComparator.java
+++ src/com/company/EmployeeSalaryComparator.java
@@ -5,6 +5,7 @@ import java.util.Comparator;
 public class EmployeeSalaryComparator implements Comparator<Employee> {
 
     public int compare(Employee e1, Employee e2) {
+
         if (e1.getSalary() > e2.getSalary()) {
             return 1;
         } else {
